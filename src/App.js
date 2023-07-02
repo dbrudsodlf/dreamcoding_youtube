9e2140a19e62aab6a4ea25561c4b38fca256bfb3
yukyung kim <contiki0527@naver.com>
2023-07-03 00:13:36 +0900

    유튜브클론1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './App.css';
-import NotFound from './pages/NotFound';
-import Root from './pages/Root';
-import Home from './pages/Home';
-
-const router = createBrowserRouter([{
-  path:'/',
-  element: <Root/>,
-  errorElement:<NotFound/>,
-  children:[
-    {index:true,element:<Home/>},
-    //{path:'videos',element:<Videos/>},
-    //{path:'videos/:videoId',element:<VideoDetail/>}
-
-  ]
-},
-// {
-//     path:'/videos',
-//     element: <Videos/>,
-// }
-]);
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
